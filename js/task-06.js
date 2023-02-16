74dce19bff7f9aaa8e69d746d32b86450289593e
Sazhin-Kostya <sazhinkostya@gmail.com>
2023-02-16 14:19:55 +0200

    Исправил дз

--- js/task-06.js
+++ js/task-06.js
@@ -7,7 +7,7 @@ const aaa = () => {
     console.log(input.value.length);
     console.log(lengthInput);
     if (input.value.length == lengthInput) {
-
+        input.classList.remove("invalid")
         input.classList.add("valid");
     } else {
         input.classList.add("invalid");
